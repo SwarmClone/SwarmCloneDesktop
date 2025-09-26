08a5a21c24eaba60a3c20367c088f62aa6eaac0f
郝某人BH <1218271192@qq.com>
2025-09-26 22:44:36 +0800

    创建项目，实现自定义标题栏

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+// import { invoke } from "@tauri-apps/api/core";
+import TitleBar from "./components/TitleBar/TitleBar.vue";
+
+// const greetMsg = ref("");
+// const name = ref("");
+//
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//   greetMsg.value = await invoke("greet", { name: name.value });
+// }
+</script>
+
+<template>
+    <TitleBar/>
+</template>
---
+++ src/components/TitleBar/TitleBar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import "./TitleBar.less"
+import { Window } from '@tauri-apps/api/window'
+import { useThemeVars } from 'naive-ui'
+
+const themeVars = useThemeVars()
+const window = Window.getCurrent()
+
+const minimize = () => {
+  window.minimize()
+}
+
+const maximize =  () => {
+  window.toggleMaximize()
+}
+
+const close = () => {
+  window.close()
+}
+</script>
+
+<template>
+<div class="title_bar" :style="{ background: `linear-gradient(90deg, ${themeVars.infoColor})` }">
+  <div class="title_bar_text" data-tauri-drag-region>
+    SwarmClone Desktop
+  </div>
+  <div class="title_bar_buttons">
+    <n-button round class="title_bar_button" id="minimize_button" @click="minimize">
+      <img src="/minimize.svg" alt="Minimize">
+    </n-button>
+    <n-button round class="title_bar_button" id="maximize_button" @click="maximize">
+      <img src="/maximize.svg" alt="Maximize">
+    </n-button>
+    <n-button round class="title_bar_button" id="close_button" @click="close">
+      <img src="/close.svg" alt="Close">
+    </n-button>
+  </div>
+</div>
+</template>
