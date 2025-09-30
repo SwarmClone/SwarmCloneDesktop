a23862619f1956df0ca2219b511ee3c0c1173561
郝某人BH <1218271192@qq.com>
2025-09-30 19:58:04 +0800

    添加许可证信息

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
+# SwarmClone Desktop
+#
+# Copyright (C) 2025 SwarmClone <https://github.com/SwarmClone> and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Eclipse Public License, Version 2.0 (EPL-2.0),
+# as published by the Eclipse Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Eclipse Public License 2.0 for more details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with this program.  For the full text of the Eclipse Public License 2.0,
+# see <https://www.eclipse.org/legal/epl-2.0/>.
+
 import sys
 import os
 from PySide6.QtWidgets import QApplication
